e31a2e7bea17a771bd8f294340933c78ea67cb5f
ihbing <shilulu@forensix.cn>
2021-01-25 17:30:25 +0800

    Init Version

---
+++ app/src/main/java/com/github/ihbing/frida/installer/XposedBaseActivity.java
@@ -0,0 +1,49 @@
+package com.github.ihbing.frida.installer;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.StringRes;
+import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
+
+import com.github.ihbing.frida.installer.util.ThemeUtil;
+
+public abstract class XposedBaseActivity extends AppCompatActivity {
+    public int mTheme = -1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceBundle) {
+        super.onCreate(savedInstanceBundle);
+        ThemeUtil.setTheme(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        ThemeUtil.reloadTheme(this);
+    }
+
+    @SuppressWarnings("deprecation")
+    public void setFloating(android.support.v7.widget.Toolbar toolbar, @StringRes int details) {
+        boolean isTablet = getResources().getBoolean(com.github.ihbing.frida.installer.R.bool.isTablet);
+        if (isTablet) {
+            WindowManager.LayoutParams params = getWindow().getAttributes();
+            params.height = getResources().getDimensionPixelSize(com.github.ihbing.frida.installer.R.dimen.floating_height);
+            params.width = getResources().getDimensionPixelSize(com.github.ihbing.frida.installer.R.dimen.floating_width);
+            params.alpha = 1.0f;
+            params.dimAmount = 0.6f;
+            params.flags |= 2;
+            getWindow().setAttributes(params);
+
+            if (details != 0) {
+                toolbar.setTitle(details);
+            }
+            toolbar.setNavigationIcon(com.github.ihbing.frida.installer.R.drawable.ic_close);
+            setFinishOnTouchOutside(true);
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setStatusBarColor(getResources().getColor(com.github.ihbing.frida.installer.R.color.colorPrimaryDark));
+        }
+    }
+}
---
+++ app/src/main/java/com/github/ihbing/frida/installer/util/RunnableWithParam.java
@@ -0,0 +1,5 @@
+package com.github.ihbing.frida.installer.util;
+
+public interface RunnableWithParam<T> {
+    public void run(T param);
+}
